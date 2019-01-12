565d14f58ad87477142f82f58cc1289bcc28b900
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-01-12 13:44:50 +0200

    onCreate(): now calls loadDebug()

--- app/src/main/java/fi/tamk/tiko/tictactoe/TicTacToe.java
+++ app/src/main/java/fi/tamk/tiko/tictactoe/TicTacToe.java
@@ -9,5 +9,6 @@ public class TicTacToe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
+        Debug.loadDebug(this);
     }
 }
