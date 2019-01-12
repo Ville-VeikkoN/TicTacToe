6f4afaa943993f4aca720e3279fbc9b50d8a949f
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-01-12 13:30:09 +0200

    Debug: fixed class name, add print(), add variables

--- app/src/main/java/fi/tamk/tiko/tictactoe/Debug.java
+++ app/src/main/java/fi/tamk/tiko/tictactoe/Debug.java
@@ -1,4 +1,10 @@
 package fi.tamk.tiko.tictactoe;
 
-public class Debuggit {
+public class Debug {
+    public static int DEBUG_LEVEL;
+    public static boolean DISPLAY_LOG_UI;
+
+    public static void print() {
+
+    }
 }
